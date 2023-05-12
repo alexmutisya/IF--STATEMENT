2235fbba7351f75b5260d7a861967cd52458fa03
alex mutisya <alexrichamerican@gmail.com>
2023-05-12 11:25:10 +0300

    IF STATEMENT
    IF IN KOTLIN

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,45 @@
+fun main() {
+    val numberofteachers = 40
+    val numberofdoctors = 17
+    if (numberofteachers > numberofdoctors) {
+        println("more teachers")
+    } else {
+        println("more doctors")
+    }
+
+
+    val interns = 80
+    if (interns == 0) {
+        println("zero interns")
+    }
+    if (interns >= 80) {
+        println("more interns turnout")
+    } else {
+        println("low interns turnout")
+    }
+
+
+    val tom = 34
+    val jane = 22
+    if (jane > tom) {
+        println("jane is older than tom")
+    } else {
+        println("tom is older than jane")
+    }
+
+
+
+    val form1=80
+    val form2=72
+    val form3=92
+    if(form1>form2){println("form 1 is bigger than form 2")}
+    if(form2>form3){println("form 2 is bigger than form 3")}
+    else{
+        println("form 3 is bigger")
+    }
+
+
+
+
+
+}
